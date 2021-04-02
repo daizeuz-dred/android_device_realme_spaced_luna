9e47b8e0543fcb122214093dcaf54b4a87522a4f
Hridya Valsaraju <hridya@google.com>
2021-04-02 11:33:45 -0700

    spaced: Set PRODUCT_SET_DEBUGFS_RESTRICTIONS
    Starting with Android R launched devices, debugfs cannot be mounted in
    production builds. In order to avoid accidental debugfs dependencies
    from creeping in during development with userdebug/eng builds, the
    build flag PRODUCT_SET_DEBUGFS_RESTRICTIONS can be set by vendors to
    enforce additional debugfs restrictions for userdebug/eng builds. The
    same flag will be used to enable sepolicy neveallow statements to
    prevent new permissions added for debugfs access.
    
    Test: build, boot
    Bug: 184381659
    Change-Id: I45e6f20c886d467a215c9466f3a09965ff897d7e

--- device.mk
+++ device.mk
@@ -200,6 +200,9 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_spaced)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
+# Kernel
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Keymaster
 PRODUCT_PACKAGES += \
    android.hardware.keymaster-V3-ndk_platform.vendor \
