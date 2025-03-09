1e652f2c54a297b7f010ae9d90a3472d83877f43
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    spaced: Shim libtflite_mtk with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.sh
+++ extract-files.sh
@@ -63,7 +63,11 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
              "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
              ;;
-        vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/lib*/libnvram.so|vendor/lib*/libsysenv.so)
+        vendor/lib*/libnvram.so|\
+        vendor/lib*/libsysenv.so|\
+        vendor/lib*/libtflite_mtk.so|\
+        odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|\
+        vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron)
             [ "$2" = "" ] && return 0
             grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
             ;;
