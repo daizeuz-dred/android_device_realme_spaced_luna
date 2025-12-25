e8ceab177fec229f9bf1ba870e2929021b2fc6e5
basamaryan <basam.aryan@gmail.com>
2025-12-25 15:13:20 +0000

    spaced: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -304,6 +304,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     FrameworksResOverlaySpaced \
+    LauncherOverlaySpaced \
     SettingsOverlaySpaced \
     SettingsProviderResOverlaySpaced \
     SystemUIOverlaySpaced \
