45637f1dc7b12a53c0911b8c33b6f7e425309987
HELLINFIX <mridulsharma57570@gmail.com>
2026-02-28 11:43:44 +0000

    Revert "spaced: init: Override dalvik heap config based on total RAM"
    This reverts commit c72ef40ae1642903837dfe6287462271acb50f0a.

--- device.mk
+++ device.mk
@@ -198,9 +198,6 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidlmemory.vendor
 
-# Init
-$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_spaced)
-
 # Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
